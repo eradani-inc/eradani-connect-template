171936dab33e8e17a3d3ccb6cd87c0f2ac86bd40
David Romo <dave@eradani.com>
2020-10-30 12:08:23 -0700

    Use Pascal case for LnxCalcItf

---
+++ src/models/LnxCalcItf.ts
@@ -0,0 +1,34 @@
+// Module LnxCalcItf
+
+import eradaniConnect from '@eradani-inc/eradani-connect';
+import configService from '../../config';
+
+const { dataTypes } = eradaniConnect;
+const config = configService.get().eradaniConnect.native;
+
+// exported func call
+export default new eradaniConnect.run.Pgm('LINUX_CALC', {
+    lib: config.objlib,
+    params: [
+        {
+            name: 'IBMICORES',
+            type: new dataTypes.PackedDecimal(15, 0)
+        },
+        {
+            name: 'LINUXSERVERS',
+            type: new dataTypes.PackedDecimal(16, 0),
+            outputOnly: true
+        }
+    ]
+});
+
+// input interface
+export interface LnxCalcItfInput {
+    IBMICORES: number;
+}
+
+// output interface
+export interface LnxCalcItfOutput {
+    IBMICORES: number;
+    LINUXSERVERS: number;
+}
