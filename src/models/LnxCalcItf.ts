bdce78cdf47994dcae3fb498d253b0333e229f45
David Romo <dave@eradani.com>
2020-11-11 09:59:41 -0800

    Fix FQN of objlib

--- src/models/LnxCalcItf.ts
+++ src/models/LnxCalcItf.ts
@@ -1,37 +1,37 @@
 // Module LnxCalcItf
 
-import eradaniConnect from "@eradani-inc/eradani-connect";
-import configService from "../../config";
+import eradaniConnect from '@eradani-inc/eradani-connect';
+import configService from '../../config';
 
 const { dataTypes } = eradaniConnect;
 const config = configService.get();
 
 // Exported function call
-export default new eradaniConnect.run.Pgm("LINUX_CALC", {
-  lib: eradaniConnect.native.config.objlib,
-  mode: "ile",
-  keepWhitespace: false,
-  params: [
-    {
-      name: "IBMICORES",
-      type: new dataTypes.PackedDecimal(15, 0)
-    },
-    {
-      name: "LINUXSERVERS",
-      type: new dataTypes.PackedDecimal(16, 0),
-      outputOnly: true
-    }
-  ]
+export default new eradaniConnect.run.Pgm('LINUX_CALC', {
+    lib: config.eradaniConnect.native.objlib,
+    mode: 'ile',
+    keepWhitespace: false,
+    params: [
+        {
+            name: 'IBMICORES',
+            type: new dataTypes.PackedDecimal(15, 0)
+        },
+        {
+            name: 'LINUXSERVERS',
+            type: new dataTypes.PackedDecimal(16, 0),
+            outputOnly: true
+        }
+    ]
 });
 
 // Input interface
 export interface LnxCalcItfInput {
-  IBMICORES: number;
-  LINUXSERVERS?: never;
+    IBMICORES: number;
+    LINUXSERVERS?: never;
 }
 
 // Output interface
 export interface LnxCalcItfOutput {
-  IBMICORES: number;
-  LINUXSERVERS: number;
+    IBMICORES: number;
+    LINUXSERVERS: number;
 }
